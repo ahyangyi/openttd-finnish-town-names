ae41ce11d387c865c3c8de3b04b0dcea14be4920
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-05-27 22:16:31 +0800

    Quick hacks to get a working nml

---
+++ nmlgen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import jinja2
+
+
+def main():
+    with open("names/real.csv") as f:
+        town_names = list(w.strip() for w in f)
+    with open("parts/prefix.csv") as f:
+        prefixes = list(w.strip() for w in f)
+    with open("parts/suffix.csv") as f:
+        suffixes = list(w.strip() for w in f)
+
+    environment = jinja2.Environment(loader=jinja2.FileSystemLoader("nml-template/"))
+    context = {
+        "town_names": town_names,
+        "prefixes": prefixes,
+        "suffixes": suffixes,
+    }
+
+    with open("nml-project/finnish_town_names.nml", "w") as f:
+        template = environment.get_template("template.nml")
+        f.write(template.render(context))
+
+
+if __name__ == "__main__":
+    main()
