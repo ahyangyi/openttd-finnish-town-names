9c3c348b92e5f100fb2bc0627553de0500f849c6
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-05-27 22:53:38 +0800

    Generate everything with template

--- nmlgen.py
+++ nmlgen.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import jinja2
+import glob
 
 
 def main():
@@ -10,16 +11,19 @@ def main():
     with open("parts/suffix.csv") as f:
         suffixes = list(w.strip() for w in f)
 
-    environment = jinja2.Environment(loader=jinja2.FileSystemLoader("nml-template/"))
+    environment = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
     context = {
         "town_names": town_names,
         "prefixes": prefixes,
         "suffixes": suffixes,
     }
 
-    with open("nml-project/main.nml", "w") as f:
-        template = environment.get_template("main.nml")
-        f.write(template.render(context))
+    for path in glob.glob("nml-template/**/*"):
+        new_path = "nml-project" + path[len("nml-template"):]
+
+        with open(new_path, "w") as f:
+            template = environment.get_template(path)
+            f.write(template.render(context))
 
 
 if __name__ == "__main__":
